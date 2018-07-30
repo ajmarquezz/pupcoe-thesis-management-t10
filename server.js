2475fbf1c0a1322320665b4c5c5f8e638eee9bbc
Alfonso <marquez.josealfonso@gmail.com>
2018-07-30 15:29:28 +0800

    fixed issues

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/', function(req,res) {
 
 app.get('/details/:id', (req,res)=>{
   var id = req.params.id;
-  client.query('SELECT * FROM Products ORDER BY id', (req, data)=>{
+  client.query('SELECT * FROM products', (req, data)=>{
     var list = [];
 		for (var i = 0; i < data.rows.length; i++) {
 				list.push(data.rows[i]);
