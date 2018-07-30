3d4f4ad7830ff642380c9d2b357bde55cb87a58a
Alfonso <marquez.josealfonso@gmail.com>
2018-07-30 08:00:16 +0800

    error debugging

--- server.js
+++ server.js
@@ -35,8 +35,7 @@ app.get('/', function(req, res){
         // imageUrl: '/img.jpg'
         //   }
       ,results);
-
-      
+ 
     })
     .catch((err) => {
       console.log('error', err);
@@ -65,32 +64,33 @@ app.get('/details', function(req, res){
 
 
 //POST route from contact form
-app.post('/contact', function (req, res) {
-  let mailOpts, smtpTrans;
-  smtpTrans = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: GMAIL_USER,
-      pass: GMAIL_PASS
-    }
-  });
-  mailOpts = {
-    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
-    to: GMAIL_USER,
-    subject: 'New message from contact form at tylerkrys.ca',
-    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
-  };
-  smtpTrans.sendMail(mailOpts, function (error, response) {
-    if (error) {
-      res.render('contact-failure');
-    }
-    else {
-      res.render('contact-success');
-    }
-  });
-});
+// app.post('/contact', function (req, res) {
+//   let mailOpts, smtpTrans;
+//   smtpTrans = nodemailer.createTransport({
+//     host: 'smtp.gmail.com',
+//     port: 465,
+//     secure: true,
+//     auth: {
+//       user: GMAIL_USER,
+//       pass: GMAIL_PASS
+//     }
+//   });
+//   mailOpts = {
+//     from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+//     to: GMAIL_USER,
+//     subject: 'New message from contact form at tylerkrys.ca',
+//     text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+//   };
+
+//   smtpTrans.sendMail(mailOpts, function (error, response) {
+//     if (error) {
+//       res.render('contact-failure');
+//     }
+//     else {
+//       res.render('contact-success');
+//     }
+//   });
+// });
 
 
 
