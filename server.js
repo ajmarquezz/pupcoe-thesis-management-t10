a7e966903d27e3e3b88945cece86c14f18b3257c
Patricia <patpatnastick@gmail.com>
2018-07-30 16:14:57 +0800

    Mailer fix and getting from database fix

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/', function(req,res) {
 	client.query('SELECT * FROM products ORDER BY id', (req, data)=>{
 		var list = [];
 		for (var i = 0; i < data.rows.length; i++) {
-			list.push(data.rows[i]);
+			list.push(data.rows[i-1]);
 		}
 		res.render('products',{
 			data: list,
